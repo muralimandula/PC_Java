78bc19c40181a82432daba72895203af234fd8d5
muralimandula <murali.mandula@msitprogram.net>
2019-09-25 21:39:27 +0530

    Day3_A4  StringHandling  checkStyle_Done

---
+++ Day 3/A4_StringHandling/StringHandling.java
@@ -0,0 +1,31 @@
+/**
+ * Given an array of Strings, convert each string of the array into
+ * lower case String and then return the array.
+ *
+ * Input : {"Hello", "Hi"}
+ * Output : {"hello", "hi"}
+ *
+ * @author Siva Sankar
+ */
+
+final class StringHandling {
+    /**.
+     * dummay constructor
+     */
+    private StringHandling() {
+    }
+    /**
+     * This method returns the array of lower case strings.
+     * @param arr the input array.
+     * @return the array of lower case Strings based on the input parameter.
+     */
+    public static String[] lower(final String[] arr) {
+        //  your code goes here....
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].toLowerCase();
+        }
+
+        return arr;
+    }
+}
