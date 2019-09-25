e565b8c55df40789308977b3860a48fcf0c420e1
muralimandula <murali.mandula@msitprogram.net>
2019-09-25 20:05:33 +0530

    Day3_A1  StringTimes  added package-info

---
+++ Day 3/A1_StringTimes/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String str = "Abc";
+
+        for(int i = 0; i < 5; i++)
+            System.out.print(str);
+    }
+}
