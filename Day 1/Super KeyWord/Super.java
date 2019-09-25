67312405e7eed97765f9a35c6cadfa672d66b013
muralimandula <murali.mandula@msitprogram.net>
2019-09-25 19:19:37 +0530

    At last, kedup with checkstyle

--- Day 1/Super KeyWord/Super.java
+++ Day 1/Super KeyWord/Super.java
@@ -1,38 +1,80 @@
+/** hi.
+ * this is
+ * Class A */
 class A {
-  int integerA;
-  public A(int a) {     // this class A constructor() takes one argument for creation
-    integerA = a;       // assigns it to integerA [a class A variable]
-    System.out.println("In constructor A");
-  } 
+    /**.
+     * integer A*/
+    private int integerA;
+    // this class A constructor() takes one argument for creation
 
-  public void method(int a) {                               // method(argument1)
-      System.out.println("Im with one argument : "+ a);
-  }
+    A(final int a) {
+        this.integerA = a;       // assigns it to integerA [a class A variable]
+        System.out.println("In constructor A");
+    }
+
+    // method(argument1)
+    public void method(final int a) {
+        System.out.println("Im with one argument : " + a);
+    }
+
+    public int getIntegerA() {
+        return this.integerA;
+    }
 }
+
+/* Class B extending A*/
 class B extends A {
-  int integerB;
-  public B(int a,int b) {  // this class B constructor() takes two argument for creation
-    super(a);
-    integerB = b;
-    System.out.println(super.integerA + " " + integerB);
-  }
-  public void method(int a, int b) {                       // method(argument1, argument2)
-    System.out.println("Im with two argument : " + a + " " + b);
+    /**.
+     * integer B*/
+    private int integerB;
+
+    // this class B constructor() takes two argument for creation
+    /* Constructor for class A*/
+    B(final int a, final int b) {
+        super(a);
+        integerB = b;
+        System.out.println(A.getIntegerA + " " + integerB);
+    }
+
+    // method(argument1, argument2)
+    public void method(final int a, final int b) {
+        System.out.println("Im with two argument : " + a + " " + b);
+    }
+
+    public void method(final double a, final double b) {
+        System.out.println("Im with two double arguments: " + a + " " + b);
+    }
+
+    public int getIntegerB() {
+        return this.integerB;
+    }
 }
 
- public void method(double a, double b){
-    System.out.println("Im with two arguments that are double : " + a + " " + b);
- }
-}
+/*hi.
+*Super class */
+final class Super {
 
-public class Super {
-    public static void main(String[] args) {
-        B nB = new B(5, 10);
-        System.out.println(nB.integerA + "  " +nB.integerB);
+  /*
+  *constructor */
+    private Super() {
+    }
 
-        //the below teo exhibits method overloading
-        nB.method(50);                                            // calls the one with only one argument
-        nB.method(50, 100);                                       // calls the one with two arguments
-        nB.method(10.0, 2.5);                                     // calls the one with two arguments that are double
+  /* Main Function*/
+    public static void main(final String[] args) {
+        final int arg1 = 5;
+        final int arg2 = 10;
+        B nB = new B(arg1, arg2);
+        System.out.println(nB.getIntegerA() + "  " + nB.getIntegerB());
+
+        //the below calls exhibits method overloading
+
+        // calls the one with only one argument
+        nB.method(arg1);
+
+        // calls the one with two arguments
+        nB.method(arg1, arg2);
+
+        // call two arguments double
+        nB.method((double) arg1, (double) arg2);
     }
 }
