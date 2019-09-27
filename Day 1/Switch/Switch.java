f528d81a06da0f31a2a7ce3bc0896d046f7924a0
muralimandula <murali.mandula@msitprogram.net>
2019-09-27 14:23:09 +0530

    Switch sample

---
+++ Day 1/Switch/Switch.java
@@ -0,0 +1,22 @@
+class Switch {
+
+    public static void main(String[] args) {
+        int key = 1;
+        switch (key) {
+            case 1:
+                System.out.println("Im at one");
+                // break;
+
+            case 2:
+                System.out.println("Im at two");
+                break;
+
+            case 3:
+                System.out.println("Im at three");
+                break;
+ 
+            default:
+                break;
+        }
+    }
+}
