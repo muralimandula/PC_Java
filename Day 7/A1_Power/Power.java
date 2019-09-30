cf40375dee4cd2861e13ee46f61d93d562de280e
muralimandula <murali.mandula@msitprogram.net>
2019-09-30 18:15:56 +0530

    Day7_A1  Power(a, b) - Method Overloading

---
+++ Day 7/A1_Power/Power.java
@@ -0,0 +1,39 @@
+/**
+ * Given "a" an integer or float value, and b as an integer value, 
+ * Find a power b.
+ *
+ * @author Siva Sankar
+ */
+
+ public class Power {
+    /**
+     * This method finds a^b where both a and b are integers.
+     * @param  a, the base
+     * @param  b, the exponential
+     * @return a^b which is a long value.
+     */
+     public static long pow(int a, int b) {
+        return (long)Math.pow(a, b);
+     }
+
+     /**
+     * This method finds a^b where both variable a is a double 
+     * and b is an integer.
+     * @param  a, the base
+     * @param  b, the exponential
+     * @return a^b which is a long value.
+     */
+     public static double pow(float a, int b) {
+      return Math.pow(a, b);
+     }
+
+     public static double pow(double a, int b) {
+      return Math.pow(a, b);
+     }
+     public static double pow(double a, double b) {
+      return Math.pow(a, b);
+     }
+     public static float pow(float a, double b) {
+      return (float)Math.pow(a, b);
+     }
+ }
